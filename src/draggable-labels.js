627ba5dd90c0c1ff13c351bf1f29dec0f2a919ec
Balazs Ree <ree@greenfinity.hu>
2015-01-26 19:13:23 +0100

    Fix the offset error, by taking the offset as the upper bound of the two connection endpoint, instead of taking the limits of the extended canvas, which produced the issue originally.

--- src/draggable-labels.js
+++ src/draggable-labels.js
@@ -30,12 +30,24 @@
     var label = connection.getOverlay('label');
     var elLabel = label.getElement();
     instance.draggable(elLabel, {
-      drag: function(evt) {
+      drag: function() {
         var pos =  jsPlumb.getUIPosition(arguments, jsPlumb.getZoom());
-        var canvas = connection.canvas;
-        var offsetLeft = parseInt(canvas.style.left),
-          offsetTop = parseInt(canvas.style.top);
-        var closest = getLabelPosition(connection, pos.left - offsetLeft, pos.top - offsetTop);
+        // Instead of the canvas of the connection, the top left bound of the
+        // two endpoint coordinates must be considered.
+        var oe1 = jsPlumb.getElementObject(connection.endpoints[0].canvas);
+        var o1 = jsPlumbAdapter.getOffset(oe1, instance);
+        var oe2 = jsPlumb.getElementObject(connection.endpoints[1].canvas);
+        var o2 = jsPlumbAdapter.getOffset(oe2, instance);
+        // In addition, offset if with the label's midpoint, because we want
+        // to grab the middle of the label instead of the top left corner.
+        var labelWidth = label.canvas.offsetWidth;
+        var labelHeight = label.canvas.offsetHeight;
+        var o = {
+          left: Math.min(o1.left, o2.left) + labelWidth / 2,
+          top: Math.min(o1.top, o2.top) + labelHeight / 2
+        };
+        // Find the closest point on the segment.
+        var closest = getLabelPosition(connection, pos.left - o.left, pos.top - o.top);
         // Store the label's position, and repaint it.
         label.loc = closest.totalPercent;
         if (!instance.isSuspendDrawing()) {
